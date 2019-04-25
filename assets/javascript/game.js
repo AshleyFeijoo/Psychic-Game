ce190af2f608fab884cde109a1d4a0207314fe7e
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-04-25 17:45:39 -0500

    fixed issue with key presses selecting non-alpha chars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,11 +38,11 @@ var winsHtml = document.getElementById("total-wins");
 var lossesHtml = document.getElementById("total-losses");
 var guessesLeftHtml = document.getElementById("guesses-left");
 var guessedLetters = document.getElementById("guessed-letters");
+var winningLetterHtml = document.getElementById("winningLetter");
 
 
 //Program will begin by picking a random alphabetical letter
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(randomLetter);
 
 
 
@@ -53,6 +53,8 @@ document.onkeypress = function(event){
     
     var playerGuess = event.key;
     var playerGuess =event.key.toLowerCase();
+    winningLetterHtml.textContent = "Guess what letter I'm thinking of....";
+
     
 //if user input and random chosen letter are equal, the program will run this block where the user wins
 
@@ -63,12 +65,13 @@ if (!alphabet.includes(playerGuess)){
     if (playerGuess === randomLetter){
         totalWins ++; //increase wins +1
         newGuess = []; //create new empty array for letters
+        lossesHtml.innerHTML = totalLosses;
         winsHtml.innerHTML = totalWins; //puts the total wins at 1
         guessesLeft = [10]; //resets the guesses left to 
         guessesLeftHtml.innerHTML = guessesLeft; //changes the guessesLEft 
         guessedLetters.textContent = guessedLetter;
         randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(randomLetter);
+        winningLetterHtml.textContent = "The winning letter is: " + randomLetter;
         return;
     } 
     else {
@@ -89,6 +92,6 @@ if (!alphabet.includes(playerGuess)){
         guessesLeftHtml.innerHTML = guessesLeft;
         guessedLetters.innerHTML = guessedLetter;
         randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(randomLetter);
+        return;
     }
 }
