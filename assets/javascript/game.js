72018aa3ddfdd6d53062ef489aebc1d5b75c5010
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-04-25 17:28:30 -0500

    fixed issue with key presses selecting non-alpha chars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -40,7 +40,6 @@ var guessesLeftHtml = document.getElementById("guesses-left");
 var guessedLetters = document.getElementById("guessed-letters");
 
 
-
 //Program will begin by picking a random alphabetical letter
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(randomLetter);
@@ -53,21 +52,25 @@ guessesLeftHtml.innerHTML = guessesLeft;
 document.onkeypress = function(event){
     
     var playerGuess = event.key;
+    var playerGuess =event.key.toLowerCase();
     
 //if user input and random chosen letter are equal, the program will run this block where the user wins
+
+if (!alphabet.includes(playerGuess)){
+    return;
+}
 
     if (playerGuess === randomLetter){
         totalWins ++; //increase wins +1
         newGuess = []; //create new empty array for letters
         winsHtml.innerHTML = totalWins; //puts the total wins at 1
         guessesLeft = [10]; //resets the guesses left to 
-        guessesLeftHtml.innerHTML = guessesLeft;
-        guessedLetters.innerHTML = guessedLetter;
+        guessesLeftHtml.innerHTML = guessesLeft; //changes the guessesLEft 
+        guessedLetters.textContent = guessedLetter;
         randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
         console.log(randomLetter);
     } 
     else {
-
     if (newGuess.includes(playerGuess)){} //if the player guess is equal to a different guess, do nothing
     else {
     guessesLeft = guessesLeft - 1; //if player guess is not equal to a letter, remove a guess left
